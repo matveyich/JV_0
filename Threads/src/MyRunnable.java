01a3d29255a91508e930459ed5ec85935a4e9e6c
matveyich <matveycih@gmail.com>
2012-04-28 14:42:46 +0300

    Threads via implementing Runnable interface

---
+++ Threads/src/MyRunnable.java
@@ -0,0 +1,19 @@
+
+public class MyRunnable implements Runnable{
+
+	@Override
+	public void run() {
+
+		for (int i = 0; i < 100; i++) {
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println("Third thread: " + i);
+		}
+		
+	}
+
+	
+}
